1d6f3f18836cfa0ecbb94e39349b7f0e7e6f9d47
KonnyLiz <pinchehugochan@gmail.com>
2017-05-16 08:40:59 -0600

    textura en un cuadro

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <SOIL/SOIL.h> 
+
+
+GLuint texture[0];
+
+void resize(int height, int width) {
+    const float ar = (float) width / (float) height;
+    glViewport(0, 10, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 90.0);
+    //gluLookAt(0, 2, 0, -1, 1, -3, 0, 1, 0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
+}
+
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    glTranslatef(0.0f,0.0f,-5.0f);
+    
+     texture[0] = SOIL_load_OGL_texture // cargamos la imagen
+    (
+        "piso3.jpg",
+        SOIL_LOAD_AUTO,
+        SOIL_CREATE_NEW_ID,
+        SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+    );
+	// allocate a texture name
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glBindTexture(GL_TEXTURE_2D, texture[0]);
+    
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);  //expancion, de coordenadas que van a los vertices hay que comenzar de los altos, de 1, pero asi se ve  normal :v
+    glVertex3f(1.0,-1.0,-1.0);
+    glTexCoord2f(0.0f, 3.0f);  //parametros son s,t  comenzando de 0,0
+    glVertex3f(1.0,1.0,-1.0);
+    glTexCoord2f(3.0f, 3.0f);  
+    glVertex3f(-1.0,1.0,-1.0);
+    glTexCoord2f(3.0f, 0.0f);  
+    glVertex3f(-1.0,-1.0,-1.0);
+    glEnd();
+    
+    glutSwapBuffers();
+    
+   
+}
+
+int main(int argc, char **argv)
+{
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInit(&argc, argv);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("Kuro <3");
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+   
+    /////////////////////////////////////
+    glEnable(GL_TEXTURE_2D);
+    glShadeModel(GL_SMOOTH);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glutMainLoop();
+}
